c8ab8b0b2033a5191d3425955ad1e5cd17b35db3
thghca <thghca@gmail.com>
2017-10-26 22:08:33 +0500

    Removed max width patch

--- Source/FloatMenu_get_ColumnWidth_Patch.cs
+++ Source/FloatMenu_get_ColumnWidth_Patch.cs
@@ -8,35 +8,33 @@ using Verse;
 namespace StuffCount
 {
 
-    [HarmonyPatch(typeof(FloatMenu))]
-    [HarmonyPatch("get_ColumnWidth")]
-    public static class FloatMenu_get_ColumnWidth_Patch
-    {
-        public static void Postfix(FloatMenu __instance, ref float __result)
-        {
-            var _options = (List<FloatMenuOption>)Traverse.Create(__instance).Field("options").GetValue();
+    //[HarmonyPatch(typeof(FloatMenu))]
+    //[HarmonyPatch("get_ColumnWidth")]
+    //public static class FloatMenu_get_ColumnWidth_Patch
+    //{
+    //    public static void Postfix(FloatMenu __instance, ref float __result)
+    //    {
+    //        var _options = (List<FloatMenuOption>)Traverse.Create(__instance).Field("options").GetValue();
 
-            float MinimumColumnWidth = (float)Traverse.Create(typeof(FloatMenu)).Field("MinimumColumnWidth").GetValue();
-            float MaximumColumnWidth = 600f;
+    //        float MinimumColumnWidth = (float)Traverse.Create(typeof(FloatMenu)).Field("MinimumColumnWidth").GetValue();
+    //        float MaximumColumnWidth = 600f;
 
-            for (int i = 0; i < _options.Count; i++)
-            {
-                float requiredWidth = _options[i].RequiredWidth;
-                if (requiredWidth >= MaximumColumnWidth)
-                {
-                    __result = MaximumColumnWidth;
-                    return;
-                }
-                if (requiredWidth > MinimumColumnWidth)
-                {
-                    MinimumColumnWidth = requiredWidth;
-                }
-            }
+    //        for (int i = 0; i < _options.Count; i++)
+    //        {
+    //            float requiredWidth = _options[i].RequiredWidth;
+    //            if (requiredWidth >= MaximumColumnWidth)
+    //            {
+    //                __result = MaximumColumnWidth;
+    //                return;
+    //            }
+    //            if (requiredWidth > MinimumColumnWidth)
+    //            {
+    //                MinimumColumnWidth = requiredWidth;
+    //            }
+    //        }
             
-            __result = Mathf.Round(MinimumColumnWidth);
-        }
-
-
-    }
+    //        __result = Mathf.Round(MinimumColumnWidth);
+    //    }
+    //}
 
 }
